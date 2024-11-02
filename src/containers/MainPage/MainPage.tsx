528b8ad105ffca3f8941bc21a07158ff5afc814f
Nelli Gorlova <nelli7035@gmail.com>
2024-11-03 02:59:23 +0600

    Created the MainPage container

---
+++ src/containers/MainPage/MainPage.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+const MainPage = () => {
+  return (
+    <>
+      <div className="bg-info-subtle">
+        <div className="container">
+          <h3 className="py-4">Calorie tracker</h3>
+        </div>
+      </div>
+      <div className="container">
+        тут будут блюда
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
