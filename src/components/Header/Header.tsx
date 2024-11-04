dfe56a033ebdd172c808619b518f92886de1214a
Nelli Gorlova <nelli7035@gmail.com>
2024-11-04 06:56:31 +0600

    EX-66 is done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { NavLink } from 'react-router-dom';
-import * as React from 'react';
 
 const Header = () => {
   return (
--- src/components/UI/Loader.tsx
+++ src/components/UI/Loader.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 const Loader = () => {
   return (
     <div className="container text-center m-5">
